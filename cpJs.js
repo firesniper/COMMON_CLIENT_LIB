83c99c734d2a20aaaceef891b5e5c4e133aec58c
firesniper <422261470@qq.com>
2017-06-06 21:14:01 +0800

    Update cpJs.js

--- cpJs.js
+++ cpJs.js
@@ -1,11 +1,7 @@
 let fs = require ( "fs" ) ;
-let inputFile = "public/node_js/webpack.html" ;
-let outputDir = "public/node_js/html/" ;
-let outputFile = "output.html" ;
-let outputUri = outputDir + outputFile ;
 
-let readerStream = fs.createReadStream ( inputFile ) ;
-readerStream.setEncoding ( "utf-8" ) ;
+
+
 // console.log ( "global:" , global.fs ) ;
 Object.defineProperties
 (
@@ -64,6 +60,8 @@ Object.defineProperties
             writable : true ,
             value : function ( parentNode )
             {
+                let parentNodeDef = this.indexOf ( "<head" ) > -1 ? "head" : undefined ;
+                parentNode = parentNode ? parentNode : parentNodeDef ;
                 let parentRegPg = null ;
                 switch ( parentNode )
                 {
@@ -159,121 +157,173 @@ let sourceStrA1 = [
 ] ;
 let sourceStrB1 = 
 `
-<head>
-<meta charset='utf-8' />
-<meta http-equiv='cache-control' content='no-cache' />
-<meta  content='width=device-width,name='viewport  ' height=device-height, user-scalable=no, initial-scale=1.0 ,maximum-scale=1.0, minimum-scale=1.0' />
-</head>
+
 ` ;
 console.log ( "begin" ) ;
 // console.log ( "readerStream:" , readerStream.on ) ;
 
 
 
-let promise = Promise.resolve
-(
-    readerStream.on
-    (
-        "data" ,
-        function ( chunk )
+
+
+let cpJs = 
+{
+
+    init : function ( _inputUri , _outputDir , _outputFile , sourceStrB1 ) 
+    {
+        /*let inputUri 
+        // = "public/node_js/webpack.html" 
+        ;
+        let outputDir 
+        // = "public/node_js/html/" 
+        ;
+        let outputFile 
+        // = "output.html" 
+        ;*/
+
+        let inputUri = _inputUri ;
+        let outputDir = _outputDir ;
+        let outputFile = _outputFile ;
+
+
+        let outputUri = outputDir + outputFile ;
+        let readerStream = fs.createReadStream ( inputUri ) ;
+        readerStream.setEncoding ( "utf-8" ) ;
+
+        var fnL = function ( uri  )
         {
-            console.log ( "chunk:" ,  chunk ) ;
-            /*let buf = new Buffer ( chunk ) ;
-            let buf2 = buf.toString ( "utf-8" ) ;
-            console.log ( "buf2:" ,  buf2 ) ;*/
-        /* let targetA1 = chunk.contentWrap( "head" ).content ;
+            fs.readdir 
+            (
+                uri ,
+                function ( err , dirs )
+                {
+                    let x = 2 ;
+                    let inc = 0 ;
+                    // console.log ( "dirs:" , dirs ) ;
+                    // var fnL2 = function ()
+                    // {
+                            console.log ( "dirs0:" , dirs ) ;
+                            if ( dirs == undefined ) return ;
+                            hfA01 : for ( var i = 0 ; i < dirs.length ; i++ ) 
+                            { 
+                                console.log ( "dirs[ %i ]:" , i , dirs[ i ] ) ;
+                                let Dirf = dirs [ i ] ;
+                                if ( Dirf.indexOf ( "." ) > 0 ) continue hfA01 ;
+                                console.log ( "Dirf:" , Dirf ) ;
+                                // if ( i >= dirs.length ) return ;
+                                /*fs.readdir 
+                                (
+                                    dirs [ i ] ,
+                                    function ( err , dirs2 )
+                                    {
+                                        console.log ( "dirs2:" , dirs2 ) ;
+                                        // fnL ( dirs2 ) ;
+                                    }
+                                ) ;*/
+                                if ( Dirf == undefined ) continue hfA01 ;
+                                fnL (  Dirf ) ;
+                             } ;       
+                    // } ;
+                    /*inc ++ ;
+                    if ( inc >= x ) return ;
+                    fnL ( Dirf ) ;*/
+                        
+                    
+                }
+            ) ;
+        } ;
+        fnL ( "./" ) ;
+        
+
+        let fnA01 = function ( pc )
+        {
+            console.log ( "pc:" , typeof pc ) ;
+            let targetA1 = pc.contentWrap (  ).content ;
             console.log ( "targetA1:" , targetA1 ) ;
 
             let sourceStrA2 = sourceStrA1.join ( "" ).contentWrap ().content ;
             console.log ( "sourceStrA2:" , sourceStrA2 ) ;
-            let sourceData = sourceStrB1.contentWrap ( "head" ) ;
+            let sourceData = sourceStrB1.contentWrap (  ) ;
             console.log ( "sourceData.content:" , sourceData.content ) ;
 
             let resAry = targetA1.excludeOverlap ( sourceData.content ) ;
+            console.log ( " resAry:" ,  resAry ) ;
+
+            // let resAry2 = targetA1.concat ( resAry ) ;
+            // console.log ( " resAry2:" ,  resAry2 ) ;
             let parentWrap = sourceData.parentWrap ;
-            let resData = parentWrap[ 0 ] + resAry + parentWrap[ parentWrap.length - 1 ] ;
-            console.log ( "resAry:" , resData ) ;*/
-            /*let writerStream = fs.createWriteStream ( outputFile ) ;
-            writerStream.write ( resData , "utf8" ) ;
-            writerStream.end () ;
-            writerStream.on
+            let headStr4 = ( parentWrap[ 0 ] + "\n" + resAry.join( "\n" ) + "\n" + parentWrap[ parentWrap.length - 1 ] ) ;
+            console.log ( "headStr4:" , headStr4 ) ;
+            let resData = pc.placeHolder( /.*/ig ) ;
+            let resData2 = resData[ 0 ].replace ( /<head.*>.*<\/head>/ig , headStr4 ) ;
+            console.log ( "resData2:" , resData2.replace ( /\$placeHolderA1/ig , "\n" ) ) ;
+            fs.existsSync 
             (
-                "finish" ,
-                function ()
+                outputDir
+                ,
+                function ( flag )
                 {
-                    console.log ( "finish" ) ;
+                    if ( flag ) return ; 
+                    fs.mkdirSync ( outputDir ) ;
+
                 }
-            ) ;*/
-        }
-    ) 
-) ;
+            ) ;
+            fs.writeFile 
+            ( 
+                outputUri , 
+                function ( a ) 
+                {
+                    console.log ( "outputUri:" , outputUri ) ;
+                    let writerStream = fs.createWriteStream ( outputUri ) ;
+                    
+                    writerStream.write ( resData2.replace ( /(?:\$placeHolderA1){1,}/ig , "\n" ) , "utf8" ) ;
+                    writerStream.end () ;
+                    writerStream.on
+                    (
+                        "finish" ,
+                        function ()
+                        {
+                            console.log ( "finish" ) ;
+                        }
+                    ) ;
+                } 
+            ) ;
+            
+        } ;
 
-promise.then
-(
-    function ( a )
-    {
-        // console.log ( "a:" , a ) ;
-        a.on
+        let promise = Promise.resolve
         (
-            "data" ,
-            function ( pc )
-            {
-                console.log ( "pc:" , typeof pc ) ;
-                let targetA1 = pc.contentWrap( "head" ).content ;
-                console.log ( "targetA1:" , targetA1 ) ;
-
-                let sourceStrA2 = sourceStrA1.join ( "" ).contentWrap ().content ;
-                console.log ( "sourceStrA2:" , sourceStrA2 ) ;
-                let sourceData = sourceStrB1.contentWrap ( "head" ) ;
-                console.log ( "sourceData.content:" , sourceData.content ) ;
-
-                let resAry = targetA1.excludeOverlap ( sourceData.content ) ;
-                console.log ( " resAry:" ,  resAry ) ;
-
-                // let resAry2 = targetA1.concat ( resAry ) ;
-                // console.log ( " resAry2:" ,  resAry2 ) ;
-                let parentWrap = sourceData.parentWrap ;
-                let headStr4 = ( parentWrap[ 0 ] + "\n" + resAry.join( "\n" ) + "\n" + parentWrap[ parentWrap.length - 1 ] ) ;
-                console.log ( "headStr4:" , headStr4 ) ;
-                let resData = pc.placeHolder( /.*/ig ) ;
-                let resData2 = resData[ 0 ].replace ( /<head.*>.*<\/head>/ig , headStr4 ) ;
-                console.log ( "resData2:" , resData2.replace ( /\$placeHolderA1/ig , "\n" ) ) ;
-                fs.existsSync 
-                (
-                    outputDir
-                    ,
-                    function ( flag )
-                    {
-                        if ( flag ) return ; 
-                        fs.mkdirSync ( outputDir ) ;
-
-                    }
-                ) ;
-                fs.writeFile 
-                ( 
-                    outputUri , 
-                    function ( a ) 
-                    {
-                        console.log ( "outputUri:" , outputUri ) ;
-                        let writerStream = fs.createWriteStream ( outputUri ) ;
-                        
-                        writerStream.write ( resData2.replace ( /(?:\$placeHolderA1){1,}/ig , "\n" ) , "utf8" ) ;
-                        writerStream.end () ;
-                        writerStream.on
-                        (
-                            "finish" ,
-                            function ()
-                            {
-                                console.log ( "finish" ) ;
-                            }
-                        ) ;
-                    } 
-                ) ;
+            readerStream.on
+            (
+                "data" ,
+                function ( chunk , fnA01 )
+                {
+                    console.log ( "chunk:" ,  chunk ) ;
                 
-            } 
+                }
+            ) 
         ) ;
-    }
-    ,
-    function ()
-    {}
-) ;
+
+        promise.then
+        (
+            function ( readerStream )
+            {
+                // console.log ( "readerStream:" , readerStream ) ;
+                // console.log ( "b:" , b ) ;
+                readerStream.on
+                (
+                    "data" ,
+                    fnA01
+                ) ;
+            } 
+            ,
+            function ()
+            {
+
+            }
+        ) ;
+
+    } 
+} ;
+
+module.exports = cpJs ;
