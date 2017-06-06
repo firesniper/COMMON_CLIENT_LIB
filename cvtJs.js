a280763fbeb40f0cb711e89809d97b1fa36461e7
firesniper <422261470@qq.com>
2017-06-06 21:12:18 +0800

    a2

--- cvtJs.js
+++ cvtJs.js
@@ -1,122 +1,329 @@
-Object.defineProperties 
+let fs = require ( "fs" ) ;
+
+
+
+// console.log ( "global:" , global.fs ) ;
+Object.defineProperties
 (
     String.prototype ,
     {
-        "formatToRegStr" : {
+        "placeHolder" : {
+            enumerable : false ,
+            configurable : true ,
+            writable : true ,
+            value : function ( parentRegPg , b )
+            {
+                let args = Array.prototype.slice.call ( arguments ) ;
+                let $this = args[ 1 ] ? args[ 1 ] : this ;
+                return $this
+                        .toString ()
+                        .replace ( /(?:\n|\r)/ig , "$placeHolderA1" ) 
+                        .match ( /[^\f\n\r\t\v]/ig )
+                        .join ( "" )
+                        .match 
+                        ( 
+                            // new RegExp( parentRegPg.a , "ig" ) 
+                            parentRegPg
+                        ) ;
+            }
+        } ,
+        "rSpace_aNl" : {
             enumerable : false ,
             configurable : true ,
             writable : true ,
             value : function ( a )
             {
-                var defCharAry = [ "/" , "\\" ] ; 
-                // console.log ( "this:" , this ) ;
-                var thisAry = this.split ( "" ) ;
-                for ( var i = 0 ; i < thisAry.length ; i++ )
+                let args = Array.prototype.slice.call ( arguments ) ;
+                let $this = args[ 0 ] ? args[ 0 ] : this ;
+                return $this
+                // .replace ( />.*</ig , ">\n<" ) 
+                // .replace ( /^.*</ig , "<" )
+                // .replace ( /\/.*>.*$/ig , ">" )
+                // .replace ( /\/>.*</ig , "/>\n<" ) ;
+            }
+        } ,
+        "caseQuote" : {
+            enumerable : false ,
+            configurable : true ,
+            writable : true ,
+            value : function ( a )
+            {
+                let args = Array.prototype.slice.call ( arguments ) ;
+                let $this = args[ 0 ] ? args[ 0 ] : this ;
+                return $this.toLowerCase().replace ( /(?:\'|\")/ig , "" ).replace ( / /ig , "" )  ;
+            }
+        } 
+        ,
+        "contentWrap" : {
+            enumerable : false ,
+            configurable : true ,
+            writable : true ,
+            value : function ( parentNode )
+            {
+                let parentNodeDef = this.indexOf ( "<head" ) > -1 ? "head" : undefined ;
+                parentNode = parentNode ? parentNode : parentNodeDef ;
+                let parentRegPg = null ;
+                switch ( parentNode )
                 {
-                    thisAry[ i ] =  "\\" + thisAry[ i ] ;
+                    case "head" :
+                        parentRegPg = 
+                        { 
+                            a :  "<head.*>.*<\\/head>"  ,
+                            b :  "(?:<head>|<\\/head>)" 
+                        } ;
+                    break ;
+                    case "body" :
+                        parentRegPg = 
+                        { 
+                            a :  "<body.*>.*<\\/body>"  ,
+                            b :  "(?:<body>|<\\/body>)"
+                        } ;
+                    break ;
+ 
+                    case undefined :
+                        parentRegPg = 
+                        { 
+                            a :  ".*"  ,
+                            b : ""
+                        } ;
+                    break ;
+ 
                 } ;
-                // this = thisAry.join ( "" ) ;
-                return thisAry.join ( "" ) ;
-                /*var reg = new RegExp 
-                ( 
-                    "[" + defCharAry.join( "" ) + "]" , 
-                    "ig" 
-                ) ;
-                var defCharStr = this ;
-                for ( var i = 0 ; i < defCharAry.length ; i++ )
-                {
-                    defCharStr = defCharStr.replace 
-                    ( 
-                        reg , "\\" + defCharAry[ i ] 
-                    ) ;
-                } ;*/
+                let args = Array.prototype.slice.call ( arguments ) ;
+                let $this = this ;
                 
+                if (  $this.constructor.name != "String" )
+                { 
+                    throw new TypeError ( "is\'nt String type" ) ; 
+                    // return ;
+                } ;
+                // $this = $this.caseQuote () ;
+                let headStr = $this.placeHolder( new RegExp( parentRegPg.a , "ig" ) ) ;
+                console.log ( "headStr:" , headStr ) ;
+                let parentWrap = headStr[ 0 ].match ( new RegExp ( parentRegPg.b , "ig" ) ) ;
+                let headStr2 = headStr[ 0 ]
+                .replace 
+                (  
+                    new RegExp ( parentRegPg.b , "ig" ) != /(?:)/ig ?
+                    new RegExp ( parentRegPg.b , "ig" ) : "" 
+                    , 
+                    "" 
+                )
+                // .rSpace_aNl ( ) ;
+                // console.log ( "headStr2:" , headStr2 ) ;
+                console.log ( "headStr2:" , headStr2 ) ;
+                let headStr3 = headStr2.split ( "\n" ) ; 
+                // console.log ( "headStr3:" , headStr3 ) ;
+                return { 
+                    "content" : headStr3 ,
+                    "parentWrap" : parentWrap
+                } ;
             }
         }
     }
-) ;  
-var fs = require ( "fs" ) ;
-var inputFile = "./public/node_js/input.txt" ;
-
-var  iptRs = fs.createReadStream ( inputFile ) ;
-iptRs.setEncoding ( "utf-8" ) ;
-iptRs.on (
-    "data" ,
-    function ( chunk )
-    {
-        // console.log ( "chunk :" , chunk ) ;
-        // chunk = chunk.toString() ;
-        // console.log ( typeof chunk ) ;
-        var reserved0 = { "placeHolder1" : "\\//" } ; 
-        console.log ( "window:" , Object.defineProperties ) ;    
-        console.log ( "Array:" , Array ) ; 
-        // var reserved = { "placeHolder1" : /\\\/\//ig }  ;
-        var reserved = {} ;
-        for ( var pA in reserved0  )
-        {
-            var RegStr = reserved0[ pA ].formatToRegStr () ;
-            reserved[ pA ] =  new RegExp ( RegStr , "ig" ) ;
-        } ;
-        console.log ( "reserved0 Format: " , reserved0[ "placeHolder1" ].formatToRegStr (  ) ) ; 
-        console.log ( reserved[ "placeHolder1" ].test ( chunk ) ) ;
-        // console.log ( reserved[ "placeHolder1" ].exec ( chunk ) ) ;
-      /*  var matchRes = "" ;
-        matchRes = chunk.match ( reserved[ "placeHolder1" ] ) ;
-        var resChunk = chunk.replace 
-        ( 
-             /\\\/\//ig  , 
-            "placeHolder1" 
-        ) ; */
-        var resChunk = "" ;
-        for ( var p in reserved )
-        {
-            /*console.log ( "p:" , p ) ;
-            console.log ( "reserved[ p ] :" , reserved[ p ] ) ;*/
-            // var regReserved = new RegExp () ;
-            // var resServeReg = new RegExp ( reserved[ p ] , "ig" ) ;
-            // console.log ( reserved[ p ].test ( chunk ) ) ;
-            resChunk = chunk.replace ( reserved[ p ] , p ) ; 
-             
-        } ;
-        // console.log ( "matchRes:" , matchRes ) ;
-        // console.log ( "resChunk:" , resChunk ) ;
-        var regStr = 
-        [ 
-            "console.log(.*).*\\r\\n" 
-            ,
-            "\\/\\/.*\\r\\n" 
-            , 
-            "\\/\\*.*\\*\\/"
-        ] ;
-        var regStr2 = 
-        [ 
-            /console.log\(.*\).*(;|\r\n)/ig 
-            ,
-            /\/\/.*\r\n/ig
-            , 
-            /\/\*.*\*\//ig 
-        ] ;
-           
-        var regRes = resChunk ;
-        for ( var i = 0 ; i < regStr.length ; i++ )
-        {
-            //  var reg = new RegExp ( regStr[ i ] , "ig" ) ;
-            //  regRes += chunk.match ( reg , "" ) ;
-             regRes = regRes.replace ( regStr2[ i ] , "" ) ;
-        } ;
-        
-        var resChunk2 = regRes ;
-        for ( var p in reserved0 )
-        {
-            // var regReserved = new RegExp () ;
-            resChunk2 = resChunk2.replace ( new RegExp( p , "ig" ) , reserved0[ p ] ) ; 
-        } ;
-        console.log ( "resChunk2：" , resChunk2 ) ;
-        /*console.log ( "regRes:" , reserved[ "placeHolder1" ].test( regRes ) ) ;
-        console.log ( "regRes:" , new RegExp ( Object.keys( reserved ) [ 0 ] ).test( regRes ) ) ;*/
-    }
+    
 ) ;
-Promise.resolve
+Object.defineProperties
 (
-
+    Array.prototype ,
+    {
+        "excludeOverlap" : {
+            enumerable : false ,
+            configurable : true ,
+            writable : true ,
+            value : function ( bAry , aAry )
+            {
+                let args = Array.prototype.slice.call ( arguments ) ;
+                let $this = args[ 1 ] ? args[ 1 ] : this ;
+                let ary = [] ;
+                let inc = 0 ;
+                hfA01 : for ( var be = 0 ; be < $this.length ; be++ )
+                {
+                    if ( $this[ be ].caseQuote () === bAry [ be ].caseQuote () ) continue hfA01 ;
+                     
+                    ary[ inc++ ] = bAry[ be ] ;
+                } ;
+                return ary ;
+            }
+        }
+    }
+    
 ) ;
+
+let sourceStrA1 = [ 
+'<meta charset=\'utf-8\' />',
+'<meta  content=\'no-cache\' http-equiv=\'cache-control\' />',
+'<meta name=\'viewport\' content=\'width=device-width, height=device-height, user-scalable=no, initial-scale=1.0 ,minimum-scale=1.0,maximum-scale=1.0\' />' 
+] ;
+let sourceStrB1 = 
+`
+
+` ;
+console.log ( "begin" ) ;
+// console.log ( "readerStream:" , readerStream.on ) ;
+
+
+
+
+
+let cpJs = 
+{
+
+    init : function ( _inputUri , _outputDir , _outputFile , sourceStrB1 ) 
+    {
+        /*let inputUri 
+        // = "public/node_js/webpack.html" 
+        ;
+        let outputDir 
+        // = "public/node_js/html/" 
+        ;
+        let outputFile 
+        // = "output.html" 
+        ;*/
+
+        let inputUri = _inputUri ;
+        let outputDir = _outputDir ;
+        let outputFile = _outputFile ;
+
+
+        let outputUri = outputDir + outputFile ;
+        let readerStream = fs.createReadStream ( inputUri ) ;
+        readerStream.setEncoding ( "utf-8" ) ;
+
+        var fnL = function ( uri  )
+        {
+            fs.readdir 
+            (
+                uri ,
+                function ( err , dirs )
+                {
+                    let x = 2 ;
+                    let inc = 0 ;
+                    // console.log ( "dirs:" , dirs ) ;
+                    // var fnL2 = function ()
+                    // {
+                            console.log ( "dirs0:" , dirs ) ;
+                            if ( dirs == undefined ) return ;
+                            hfA01 : for ( var i = 0 ; i < dirs.length ; i++ ) 
+                            { 
+                                console.log ( "dirs[ %i ]:" , i , dirs[ i ] ) ;
+                                let Dirf = dirs [ i ] ;
+                                if ( Dirf.indexOf ( "." ) > 0 ) continue hfA01 ;
+                                console.log ( "Dirf:" , Dirf ) ;
+                                // if ( i >= dirs.length ) return ;
+                                /*fs.readdir 
+                                (
+                                    dirs [ i ] ,
+                                    function ( err , dirs2 )
+                                    {
+                                        console.log ( "dirs2:" , dirs2 ) ;
+                                        // fnL ( dirs2 ) ;
+                                    }
+                                ) ;*/
+                                if ( Dirf == undefined ) continue hfA01 ;
+                                fnL (  Dirf ) ;
+                             } ;       
+                    // } ;
+                    /*inc ++ ;
+                    if ( inc >= x ) return ;
+                    fnL ( Dirf ) ;*/
+                        
+                    
+                }
+            ) ;
+        } ;
+        fnL ( "./" ) ;
+        
+
+        let fnA01 = function ( pc )
+        {
+            console.log ( "pc:" , typeof pc ) ;
+            let targetA1 = pc.contentWrap (  ).content ;
+            console.log ( "targetA1:" , targetA1 ) ;
+
+            let sourceStrA2 = sourceStrA1.join ( "" ).contentWrap ().content ;
+            console.log ( "sourceStrA2:" , sourceStrA2 ) ;
+            let sourceData = sourceStrB1.contentWrap (  ) ;
+            console.log ( "sourceData.content:" , sourceData.content ) ;
+
+            let resAry = targetA1.excludeOverlap ( sourceData.content ) ;
+            console.log ( " resAry:" ,  resAry ) ;
+
+            // let resAry2 = targetA1.concat ( resAry ) ;
+            // console.log ( " resAry2:" ,  resAry2 ) ;
+            let parentWrap = sourceData.parentWrap ;
+            let headStr4 = ( parentWrap[ 0 ] + "\n" + resAry.join( "\n" ) + "\n" + parentWrap[ parentWrap.length - 1 ] ) ;
+            console.log ( "headStr4:" , headStr4 ) ;
+            let resData = pc.placeHolder( /.*/ig ) ;
+            let resData2 = resData[ 0 ].replace ( /<head.*>.*<\/head>/ig , headStr4 ) ;
+            console.log ( "resData2:" , resData2.replace ( /\$placeHolderA1/ig , "\n" ) ) ;
+            fs.existsSync 
+            (
+                outputDir
+                ,
+                function ( flag )
+                {
+                    if ( flag ) return ; 
+                    fs.mkdirSync ( outputDir ) ;
+
+                }
+            ) ;
+            fs.writeFile 
+            ( 
+                outputUri , 
+                function ( a ) 
+                {
+                    console.log ( "outputUri:" , outputUri ) ;
+                    let writerStream = fs.createWriteStream ( outputUri ) ;
+                    
+                    writerStream.write ( resData2.replace ( /(?:\$placeHolderA1){1,}/ig , "\n" ) , "utf8" ) ;
+                    writerStream.end () ;
+                    writerStream.on
+                    (
+                        "finish" ,
+                        function ()
+                        {
+                            console.log ( "finish" ) ;
+                        }
+                    ) ;
+                } 
+            ) ;
+            
+        } ;
+
+        let promise = Promise.resolve
+        (
+            readerStream.on
+            (
+                "data" ,
+                function ( chunk , fnA01 )
+                {
+                    console.log ( "chunk:" ,  chunk ) ;
+                
+                }
+            ) 
+        ) ;
+
+        promise.then
+        (
+            function ( readerStream )
+            {
+                // console.log ( "readerStream:" , readerStream ) ;
+                // console.log ( "b:" , b ) ;
+                readerStream.on
+                (
+                    "data" ,
+                    fnA01
+                ) ;
+            } 
+            ,
+            function ()
+            {
+
+            }
+        ) ;
+
+    } 
+} ;
+
+module.exports = cpJs ;
