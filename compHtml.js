f9149c55e9cb7defe467f6d3be0e0c67df6be570
firesniper <422261470@qq.com>
2017-06-17 19:33:35 +0800

    Update compHtml.js

--- compHtml.js
+++ compHtml.js
@@ -1,30 +1,17 @@
 let fs = require ( "fs" ) ;
 let glob = require ( "glob" ) ;
 let nodeCommonLib = require ( "../node_common_lib/node_common_lib.js" ) ;
-// console.log ( "nodeCommonLib:" , nodeCommonLib ) ;
-// nodeCommonLib.init () ;
-// console.log ( "global:" , global.fs ) ;
 
-
-/*let sourceStrA1 = [ 
-'<meta charset=\'utf-8\' />',
-'<meta  content=\'no-cache\' http-equiv=\'cache-control\' />',
-'<meta name=\'viewport\' content=\'width=device-width, height=device-height, user-scalable=no, initial-scale=1.0 ,minimum-scale=1.0,maximum-scale=1.0\' />' 
-] ;*/
-/*let sourceStrB1 = 
-`
-
-` ;*/
 console.log ( "begin" ) ;
 
 
 let cpJs = 
 {
 
-    init : function ( putPath , sourceStrB1 , globPg ) 
+    init : function ( putPath , sourceStrB1 , globPg , baseUrl ) 
     {
-         
-
+         nodeCommonLib.init ( baseUrl ) ;
+        // putPath.inputDir = putPath.inputUri.getDirFileFromUri ().dir ;
         // let inputUri = putPath.inputUri ;
        
 
@@ -72,59 +59,15 @@ let cpJs =
                     
                     
 
-
-                    var fnL = function ( uri  )
-                    {
-                        fs.readdir 
-                        (
-                            uri ,
-                            function ( err , dirs )
-                            {
-                                let x = 2 ;
-                                let inc = 0 ;
-                                // console.log ( "dirs:" , dirs ) ;
-                                // var fnL2 = function ()
-                                // {
-                                        // console.log ( "dirs0:" , dirs ) ;
-                                        if ( dirs == undefined ) return ;
-                                        hfA01 : for ( var i = 0 ; i < dirs.length ; i++ ) 
-                                        { 
-                                            // console.log ( "dirs[ %i ]:" , i , dirs[ i ] ) ;
-                                            let Dirf = dirs [ i ] ;
-                                            if ( Dirf.indexOf ( "." ) > 0 ) continue hfA01 ;
-                                            console.log ( "Dirf:" , Dirf ) ;
-                                            // if ( i >= dirs.length ) return ;
-                                            /*fs.readdir 
-                                            (
-                                                dirs [ i ] ,
-                                                function ( err , dirs2 )
-                                                {
-                                                    console.log ( "dirs2:" , dirs2 ) ;
-                                                    // fnL ( dirs2 ) ;
-                                                }
-                                            ) ;*/
-                                            if ( Dirf == undefined ) continue hfA01 ;
-                                            fnL (  Dirf ) ;
-                                        } ;       
-                                // } ;
-                                /*inc ++ ;
-                                if ( inc >= x ) return ;
-                                fnL ( Dirf ) ;*/
-                                    
-                                
-                            }
-                        ) ;
-                    } ;
-                    // fnL ( "./" ) ;
                     
 
                     let getResDataStr = function ( data , readStreamAry , inc )
                     {
                         console.log ( "data:" ,  data ) ;
-                        let targetAryA1 = data.contentWrap (  ).contentAry ;
+                        let targetAryA1 = data.contentWrap ( "head" ).contentAry ;
                         console.log ( "targetAryA1:" , targetAryA1 ) ;
                         
-                        let sourceDataPg = sourceStrB1.contentWrap (  ) ;
+                        let sourceDataPg = sourceStrB1.contentWrap ( "head" ) ;
                         console.log ( "sourceDataPg.contentAry:" , sourceDataPg.contentAry ) ;
 
                         let resDiffAry = targetAryA1.excludeOverlap ( sourceDataPg.contentAry ) ;
@@ -136,11 +79,29 @@ let cpJs =
                         let parentWrapAry = sourceDataPg.parentWrapAry ;
                         let headStr4 = ( parentWrapAry[ 0 ] + "\n" + resDiffAry2.join( "\n" ) + "\n" + parentWrapAry[ parentWrapAry.length - 1 ] ) ;
                         console.log ( "headStr4:" , headStr4 ) ;
-                        let resData = data.tokenToPlaceHolder(  ) ;
-                        let resData2 = resData.replace ( /<head.*>.*<\/head>/ig , headStr4 ) ;
-                        console.log ( "resData2:" , resData2.placeHolderToToken () ) ;
                         
-                        return resData2 ;
+                        let resData = data.tokenToPlaceHolder( "head" ) ;
+                        console.log ( "resData:" , resData ) ;
+                        let resData2 = resData.replace ( /<head.*>.*<\/head>/ig , headStr4 ) ;
+                        console.log ( "resData2:" , resData2.placeHolderToToken ( "head" ) ) ;
+                        let resData3 = resData2.placeHolderToToken ( "head" ) ;
+                        console.log ( "resData3:" , resData3.placeHolderToToken ( "head" ) ) ;
+                        
+                        /*if ( data.indexOf ( "<body" ) > -1 )
+                        {
+                            let bodyStr = data.tokenToPlaceHolder ().match ( /<body.*>.*<\/body>/ig ) ;
+                            console.log ( "bodyStr:" , bodyStr ) ;
+
+                            // let bodyStr2 = bodyStr.tokenToPlaceHolder () ;
+                            let bodyStr3 = bodyStr[ 0 ].placeHolderToToken () ;
+                            console.log ( "bodyStr3:" , bodyStr3 ) ;
+                        } ;*/
+                        let bodyStr = data.contentWrap ( "body" ).partDomStr ;
+                        let bodyStr2 = bodyStr.placeHolderToToken ( "body" ) ;
+
+                        console.log ( "bodyStr2:" , bodyStr2 ) ;
+                        
+                        return resData3 ;
                         
                     } ;
 
@@ -171,7 +132,7 @@ let cpJs =
                                         
                                 writerStream.write 
                                 ( 
-                                    resDataStr.placeHolderToToken ()  , 
+                                    resDataStr , 
                                     "utf8" 
                                 ) ;
                                 writerStream.end () ;
@@ -183,37 +144,46 @@ let cpJs =
                                         console.log ( "finish" ) ;
                                     }
                                 ) ;
-
+                                fs.unwatchFile
+                                (
+                                    $thisPath , 
+                                    function ( a , b , c )
+                                    {
+                                        console.log ( "watcha:" , a ) ;
+                                        console.log ( "watchb:" , b ) ;
+                                        console.log ( "watchc:" , c ) ;
+                                    }
+                                ) ;
                                 fs.watch 
                                 ( 
                                     $thisPath , 
-                                    function ( a , b ) 
+                                    function ( a , b , c ) 
                                     {
-                                         
+                                        console.log ( "$thisPath:" , $thisPath ) ;
+                                        
+                                        globPg = {
+                                            "cwd" : "./" ,
+                                            "regAry" : [ $thisPath , "" ]
+                                        } ;
+                                        
+                                                cpJs.init 
+                                                ( putPath , sourceStrB1 , globPg )  ;
+                                        let st01 = setTimeout 
+                                        (
+                                            function ()
+                                            {
 
-                                        cpJs.init 
-                                        ( putPath , sourceStrB1 , globPg )  ;
+                                            } ,
+                                            0
+                                        ) ;
+                                        clearTimeout ( st01 ) ;
+                                        
 
                                         
                                     } 
                                 ) ;
-                                /*fs.writeFile 
-                                ( 
-                                    // outputDir
-                                    this.path.getDirFileFromUri ().dir  
-                                    + this.path.getDirFileFromUri ().file 
-                                    + ".dev"
-                                    + this.path.getDirFileFromUri ().ext
-                                    , 
-                                    // resDataStr.replace ( /(?:\$placeHolderA1){1,}/ig , "\n" )
-                                    resDataStr.placeHolderToToken () 
-                                    ,
-                                    function ( a ) 
-                                    {
-                                        console.log ( "outputUri:" , outputUri ) ;
-                                       
-                                    } 
-                                ) ;*/
+                                
+
                             }
                             
                         ) ;
